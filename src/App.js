351a944f28c7cd01c11138922e8cfa15d1e455a7
Andres <andgonzalezcas@unal.edu.co>
2022-09-24 23:47:33 -0500

    first visible menu added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  CircleMenu,
+  CircleMenuItem,
+} from "react-circular-menu";
 
-function App() {
+import { AiOutlineTeam } from 'react-icons/ai'
+import { BiHomeAlt } from 'react-icons/bi'
+
+//colors
+//#121709
+//#173814
+//#456625
+//#7f9b2e
+//#cdd632
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="w-screen h-screen flex justify-center items-center bg-mushromm">
+      <CircleMenu
+        startAngle={-90}
+        rotationAngle={360}
+        itemSize={5}
+        radius={8}
+        rotationAngleInclusive={false}
+      >
+        <CircleMenuItem
+          onClick={() => alert("Clicked the item")}
+          tooltip={'Home'}
+          style={{ background: '#456625' }}
         >
-          Learn React
-        </a>
-      </header>
+          <BiHomeAlt className="w-10 h-10"/>
+        </CircleMenuItem>
+        <CircleMenuItem
+          onClick={() => alert("Clicked the item")}
+          tooltip={'Team'}
+          style={{ background: '#7f9b2e' }}
+        >
+          <AiOutlineTeam className="w-10 h-10"/>
+        </CircleMenuItem>
+        <CircleMenuItem
+          onClick={() => alert("Clicked the item")}
+          tooltip={'Home'}
+          style={{ background: '#456625' }}
+        >
+          A
+        </CircleMenuItem>
+        <CircleMenuItem
+          onClick={() => alert("Clicked the item")}
+          tooltip={'Home'}
+          style={{ background: '#7f9b2e' }}
+        >
+          A
+        </CircleMenuItem>
+        <CircleMenuItem
+          onClick={() => alert("Clicked the item")}
+          tooltip={'Home'}
+          style={{ background: '#456625' }}
+        >
+          A
+        </CircleMenuItem>
+        <CircleMenuItem
+          onClick={() => alert("Clicked the item")}
+          tooltip={'Home'}
+          style={{ background: '#7f9b2e' }}
+        >
+          A
+        </CircleMenuItem>
+      </CircleMenu>
     </div>
-  );
+  )
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'pine-forest': "url('./images/pine-forest-in-the-mist_1500x1000_xtrafondos.com.jpg')",
+        'mushromm': "url('./images/mushromms-glowing_3840x2553_xtrafondos.com.jpg')"
+      }
+    }
+  },
   plugins: [],
 }
