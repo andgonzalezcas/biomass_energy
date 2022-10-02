e046ac442dd43fc9910ba62d134d2b07ee73dd8a
Andres <andgonzalezcas@unal.edu.co>
2022-10-02 17:55:46 -0500

    fixed menu button route

--- src/App.js
+++ src/App.js
@@ -20,49 +20,49 @@ const App = () => {
         radius={8}
         rotationAngleInclusive={false}
       >
-        <Link to="/home">
-          <CircleMenuItem
-            tooltip={'Biomass Energy'}
-            style={{ background: '#456625' }}
-          >
-            <GiGreenPower className="w-10 h-10" />
-          </CircleMenuItem>
-        </Link>
-        <Link to={'/graph'}>
-          <CircleMenuItem
-            tooltip={'Graphs'}
-            style={{ background: '#456625' }}
-          >
-            <BsFillFileEarmarkBarGraphFill className="w-10 h-10" />
-          </CircleMenuItem >
-        </Link>
-        <Link to={'/experts'}>
-          <CircleMenuItem
-            tooltip={'Expertos'}
-            style={{ background: '#456625' }}
-          >
-            <GiBookmark className="w-10 h-10" />
-          </CircleMenuItem>
-        </Link>
-        <Link to="/team">
-          <CircleMenuItem
-            tooltip={'Team'}
-            style={{ background: '#7f9b2e' }}
-          >
-            <AiOutlineTeam className="w-10 h-10" />
-          </CircleMenuItem>
-        </Link>
+        <CircleMenuItem
+          tooltip={'Biomass Energy'}
+          style={{ background: '#456625' }}
+        >
+          <Link to="/home" className="w-28 h-28 flex justify-center items-center rounded-full">
+            <GiGreenPower className="w-12 h-12" />
+          </Link>
+        </CircleMenuItem>
+        <CircleMenuItem
+          tooltip={'Graphs'}
+          style={{ background: '#456625' }}
+        >
+          <Link to={'/graph'} className="w-28 h-28 flex justify-center items-center rounded-full">
+            <BsFillFileEarmarkBarGraphFill className="w-12 h-12" />
+          </Link>
+        </CircleMenuItem >
+        <CircleMenuItem
+          tooltip={'Expertos'}
+          style={{ background: '#456625' }}
+        >
+          <Link to={'/experts'} className="w-28 h-28 flex justify-center items-center rounded-full">
+            <GiBookmark className="w-12 h-12" />
+          </Link>
+        </CircleMenuItem>
+        <CircleMenuItem
+          tooltip={'Team'}
+          style={{ background: '#7f9b2e' }}
+        >
+          <Link to="/team" className="w-28 h-28 flex justify-center items-center rounded-full">
+            <AiOutlineTeam className="w-12 h-12" />
+          </Link>
+        </CircleMenuItem>
         <CircleMenuItem
           tooltip={'Alien mi so'}
           style={{ background: '#7f9b2e' }}
         >
-          <WiAlien className="w-10 h-10" />
+          <WiAlien className="w-12 h-12" />
         </CircleMenuItem>
         <CircleMenuItem
           tooltip={'Posibles soluciones'}
           style={{ background: '#7f9b2e' }}
         >
-          <FiPackage className="w-10 h-10" />
+          <FiPackage className="w-12 h-12" />
         </CircleMenuItem>
       </CircleMenu>
     </div>
